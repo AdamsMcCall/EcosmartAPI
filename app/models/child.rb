928a4e37c2f0f6449c146230b828efcc920fde0b
Adam Akkari <adam.akkari@epitech.eu>
2018-07-10 16:29:03 +0000

    Added basic tests

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,7 @@
 class Child < ActiveRecord::Base
-    
+  validates :account_id, presence: true
+  validates :email, presence: true
+  validates :pwd_hash, presence: true
+  validates :birthdate, presence: true
+  validates :balance, presence: true
 end
